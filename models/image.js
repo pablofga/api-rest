59cdb6cf73c717046f5af0a0a154711120301341
Pablo <quit>
2017-11-02 16:29:32 +0100

    versión inicial

---
+++ models/image.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var ImageSchema=Schema({
+    titulo:String,
+    picture:String,
+    producto:String
+});
+module.exports =mongoose.model('Image',ImageSchema);
