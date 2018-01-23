81cbb5af07e40b47a51d6035a9833c3930c6d75c
Pablo <quit>
2018-01-23 10:33:23 +0100

    23/01

--- routes/image.js
+++ routes/image.js
@@ -1,14 +1,21 @@
 'use strict'
 
 var express = require('express');
-var ImageController = require ('../controllers/image');
-var api=express.Router();
+var ImageController = require('../controllers/image');
+var api = express.Router();
 
-api.get('/prueba-image',ImageController.pruebas);
-api.get('/image/:id',ImageController.getImage);
-api.get('/images/:producto?',ImageController.getImages);
-api.post('/image',ImageController.saveImage); 
-api.put('/image/:id',ImageController.updateImage); 
-api.delete('/image/:id',ImageController.deleteImage);
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart({ uploadDir: './uploads' });
 
-module.exports=api;
+api.get('/prueba-image', ImageController.pruebas);
+api.get('/image/:id', ImageController.getImage);
+api.get('/images/:producto?', ImageController.getImages);
+api.post('/image', ImageController.saveImage);
+api.put('/image/:id', ImageController.updateImage);
+api.delete('/image/:id', ImageController.deleteImage);
+api.post('/upload-image/:id', multipartMiddleware, ImageController.uploadImage);
+api.get('/get-image2/:imageFile', multipartMiddleware, ImageController.getImageFile2);
+api.get('/get-image/:id', multipartMiddleware, ImageController.getImageFile);
+
+
+module.exports = api;
--- routes/producto.js
+++ routes/producto.js
@@ -1,13 +1,18 @@
 'use strict'
 
 var express = require('express');
-var ProductoController = require ('../controllers/producto');
-var api=express.Router();
+var ProductoController = require('../controllers/producto');
+var api = express.Router();
 
-api.get('/producto/:id',ProductoController.getProducto);
-api.get('/productos',ProductoController.getProductos);
-api.post('/producto',ProductoController.saveProducto); 
-api.put('/producto/:id',ProductoController.updateProducto); 
-api.delete('/producto/:id',ProductoController.deleteProducto);
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart({ uploadDir: './uploads' });
 
-module.exports=api;
+api.get('/producto/:id', ProductoController.getProducto);
+api.get('/productos', ProductoController.getProductos);
+api.post('/producto', ProductoController.saveProducto);
+api.put('/producto/:id', ProductoController.updateProducto);
+api.delete('/producto/:id', ProductoController.deleteProducto);
+api.post('/upload-file', multipartMiddleware, ProductoController.uploadFile);
+
+
+module.exports = api;
